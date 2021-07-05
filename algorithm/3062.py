ab6ef48ab4db719790a10b60660cff788619ee49
dajomi <kdj001106@gmail.com>
2021-07-06 02:29:11 +0900

    3062 update

---
+++ algorithm/3062.py
@@ -0,0 +1,14 @@
+iter = int(input())
+for i in range(iter) :
+    num = input()
+    slicedString = num[:: - 1]
+    hap = str(int(num) + int(slicedString))
+    begin = 0
+    end = len(hap) - 1
+    result = True
+    while begin < end:
+if hap[begin] != hap[end] : result = False
+begin += 1
+end -= 1
+if (result == True) : print("YES")
+else : print("NO")
